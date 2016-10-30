46ee9aed7566f4bf3d73c186ea78abd7d69d99a9
James Kao <james.kao@oracle.com>
2016-10-30 14:25:09 -0700

    Add some SPA and REST calls into demo app

---
+++ springboot-sample/src/main/java/com/example/springboot/rsapi/ApiConfig.java
@@ -0,0 +1,46 @@
+package com.example.springboot.rsapi;
+
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackageClasses = Api.class)
+public class ApiConfig {
+    @Autowired private Api apiService;
+ 
+    @Bean(destroyMethod = "shutdown")
+    public SpringBus cxf() {
+        return new SpringBus();
+    }
+
+    @Bean(destroyMethod = "destroy") @DependsOn("cxf")
+    public Server jaxRsServer() {
+        final JAXRSServerFactoryBean factory = new JAXRSServerFactoryBean();
+
+        factory.setServiceBean(apiService);
+        factory.setProvider(new JacksonJsonProvider());
+        factory.setBus(cxf());
+        factory.setAddress("/");
+
+        return factory.create();
+    }
+
+    @Bean
+    public ServletRegistrationBean cxfServlet() {
+        final ServletRegistrationBean servletRegistrationBean = 
+            new ServletRegistrationBean(new CXFServlet(), "/api/*");
+        servletRegistrationBean.setLoadOnStartup(1);
+        return servletRegistrationBean;
+    }
+}
---
+++ springboot-sample/src/main/resources/static/angular/app.js
@@ -0,0 +1,61 @@
+var spApp = angular.module('spApp', []);
+
+spApp.controller('mainController', function($scope, $http) {
+	
+	$scope.calcList = [];
+	$scope.symbol = "ORCL"
+	
+  $scope.doDefaultBanner = function() {
+    $scope.bannerText = "Controller Injected Banner Text";
+  };
+
+  $scope.doAlternateBanner = function() {
+    $scope.bannerText = "Welcome"
+  }
+
+  $scope.doGibberishBanner = function() {
+    $scope.bannerText = "Yoli olacid ogecox aset!"
+  }
+  
+  $scope.doInvokeMessage = function() {
+	  $http.get('/api/message').then(
+			  function(response) {
+				  $scope.bannerText = response.data.message;
+			  }
+	  );
+  }
+  
+  $scope.doInvokeHostname = function() {
+	  $http.get('/api/hostname').then(
+			  function(response) {
+				  $scope.bannerText = "Served from: "+response.data.hostname;
+			  }
+	  )
+  }
+  
+  $scope.doInvokeCalcSum = function() {
+	  $http.get('/api/randomsum', {params: {index: $scope.calcIndex}}).then(
+			  function(response) {
+				  $scope.calcList.push(response.data);
+			  },
+			  function(response) {
+				  var calcData = {
+						  size: response.data.exception,
+						  sum: response.data.error,
+						  intAtIndex: response.data.path
+				  };
+				  $scope.calcList.push(calcData);
+			  }
+	  )
+  }
+  
+  $scope.doQuote = function() {
+	  $http.get('/api/quote', {params: {symbol: $scope.symbol}}).then(
+			  function(response) {
+				  $scope.quoteText = response.data;
+			  }
+	  )
+  }
+
+  $scope.doDefaultBanner();
+});
